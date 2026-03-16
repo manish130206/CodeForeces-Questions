b40b16d7879c0a9d88972a0b9a26542598daa739
manish130206 <manishdewangan1302@gmail.com>
2026-03-16 19:59:40 +0530

    Create I. Welcome for you with Conditions.cpp

---
+++ Practice/I. Welcome for you with Conditions.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int A,B;
+    cin>>A>>B;
+    
+    if(A >= B){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
