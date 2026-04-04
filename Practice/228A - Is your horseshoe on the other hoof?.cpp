03e57dc8ff8baafce3a8f7b63e3fcab8dbcb0ef9
manish130206 <manishdewangan1302@gmail.com>
2026-04-04 17:26:14 +0530

    Add solution for 'Is your horseshoe on the other hoof?'

---
+++ Practice/228A - Is your horseshoe on the other hoof?.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int s1, s2, s3, s4;
+    cin >> s1 >> s2 >> s3 >> s4;
+
+    set<int> colors;
+    colors.insert(s1);
+    colors.insert(s2);
+    colors.insert(s3);
+    colors.insert(s4);
+
+    cout << 4 - colors.size();
+
+    return 0;
+}
